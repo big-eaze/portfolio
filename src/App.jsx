21e378237f4a6f964b7d19e79f0b2f793001bc55
Israel <israelojeleye35@gmail.com>
2025-07-25 21:39:30 +0100

    create a navBar component and style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-
-
-import "./App.css"
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { About } from "./components/About";
+import { Experience } from "./components/Experience";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
 
 function App() {
-
   return (
-    <Routes>
-     
-    </Routes>
-  )
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Experience />
+        <Projects />
+        <Contact />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+
+export function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Israel Ojeleye</div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
