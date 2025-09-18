af9ffc6d70c7486d6851409107b5d4f144055a7c
Israel <israelojeleye35@gmail.com>
2025-09-18 09:48:29 +0100

    add a new project and change the head images of each project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ export function Projects() {
       <div className="projects-grid">
 
         <div className="project-card">
-          <img src="/portfolio/project-one.jpg" alt="Project 1" />
+          <img src="/portfolio/project1.jpg" alt="Project 1" />
           <div className="project-info">
             <h3>School Management APP</h3>
             <p>A modern, visually appealing, role based(Admin, Student, staff) SM web application built with React + Express + SQLITE.</p>
@@ -29,13 +29,25 @@ export function Projects() {
         </div>
 
         <div className="project-card">
-          <img src="/portfolio/project-two.webp" alt="Project 2" />
+          <img src="/portfolio/project2.jpg" alt="Project 2" />
           <div className="project-info">
             <h3>An E-commerce website (fully responsive & user friendly)</h3>
             <p>A single-page E-commerce website with full CRUD operations, built with React + Express + SQLITE.</p>
             <div className="project-links">
               <a href="http://ginkbow-shop-env.eba-wzppp4xt.eu-north-1.elasticbeanstalk.com/" target="_blank">Live Demo</a>
               <a href="https://github.com/big-eaze/Ginkbow-shop" target="_blank" >GitHub</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="project-card">
+          <img src="/portfolio/project3.jpg" alt="Project 2" />
+          <div className="project-info">
+            <h3>A Restuarant web app (responsive to mobile screen sizes and intuitive Navigations)</h3>
+            <p>A prototype built with modern day tech.(React, tailwind-css, shadcn)</p>
+            <div className="project-links">
+              <a href="https://restaurant-ten-sable.vercel.app" target="_blank">Live Demo</a>
+              <a href="https://github.com/big-eaze/Restaurant" target="_blank" >GitHub</a>
             </div>
           </div>
         </div>
